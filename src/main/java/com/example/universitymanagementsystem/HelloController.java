9f2cab73acdadcab8135f778a41fb78f01ce9850
Ziad Teama <ziadbasaria600@gmail.com>
2024-11-27 22:45:46 +0200

    initial commit

---
+++ src/main/java/com/example/universitymanagementsystem/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.universitymanagementsystem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Elmanga 3alKamanga!");
+    }
+}
