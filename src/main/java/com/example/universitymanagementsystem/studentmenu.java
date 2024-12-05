cc51af43a8dffb331647487b604eae00f639aaf7
Ziad Teama <ziadbasaria600@gmail.com>
2024-12-05 18:55:55 +0200

    before b3basa2

--- src/main/java/com/example/universitymanagementsystem/studentmenu.java
+++ src/main/java/com/example/universitymanagementsystem/studentmenu.java
@@ -1,16 +1,4 @@
 package com.example.universitymanagementsystem;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class studentmenu extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class studentmenu {
 }
