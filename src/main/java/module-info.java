bb5501df4f83c7bd5cf270b59900f7937ff795c6
Ziad Teama <ziadbasaria600@gmail.com>
2024-12-07 19:03:34 +0200

    data retrieval is fine for student

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.universitymanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
 
+    requires java.sql;
 
     opens com.example.universitymanagementsystem to javafx.fxml;
     exports com.example.universitymanagementsystem;
