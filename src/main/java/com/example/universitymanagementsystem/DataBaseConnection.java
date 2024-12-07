3a67d59a38dc4591dfd60f076f718717f2936d9b
Ziad Teama <ziadbasaria600@gmail.com>
2024-12-07 17:19:13 +0200

    before database integrity

---
+++ src/main/java/com/example/universitymanagementsystem/DataBaseConnection.java
@@ -0,0 +1,4 @@
+package com.example.universitymanagementsystem;
+
+public class DataBaseConnection {
+}
---
+++ src/main/java/com/example/universitymanagementsystem/MainApp.java
@@ -0,0 +1,15 @@
+package com.example.universitymanagementsystem;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
